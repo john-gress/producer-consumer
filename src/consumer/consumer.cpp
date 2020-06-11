f65cb96c785f1a418ad16a233c5e649cad2128cd
John Gress <jgress.work@gmail.com>
2020-06-10 20:36:22 -0600

    Add argument processing to producer and consumer

--- src/consumer/consumer.cpp
+++ src/consumer/consumer.cpp
@@ -1,8 +1,51 @@
 #include <iostream>
 #include "pc.h"
 #include "Config.h"
+#include <unistd.h>
+#include <cstdlib>
+
+int ProcessArgs(int argc, char* argv[], int& N, std::string& match) {
+   int opt;
+   const int kMinNumBufs(10);
+   const int kExpectedArgCount(5);
+
+   if (argc != kExpectedArgCount) {
+      return 1;
+   }
+
+   while ((opt = getopt(argc, argv, "n:m:")) != -1) {
+      switch (opt) {
+         case 'n':
+            N = atoi(optarg);
+            break;
+         case 'm':
+            match = optarg;
+            break;
+         default:
+            return 2;
+      }
+   }
+
+   if (N < kMinNumBufs) {
+      std::cerr << "Minumun value for -n argument is " << kMinNumBufs << std::endl;
+      return 3;
+   }
+
+   return 0;
+}
 
 int main(int argc, char* argv[]) {
+   int N;
+   std::string match;
+
+   int res = ProcessArgs(argc, argv, N, match);
+   if (res != 0) {
+      std::cerr << "Usage: " << argv[0] << " -n [NUM_BUFS] -m [MATCH_STRING]" << std::endl;
+      exit(res);
+   }
+
+   std::cout << "Num Bufs: " << N << ", match: " << match << std::endl;
+
    pc myPc;
    Config myConfig("../conf/pcConf");
 
--- src/producer/producer.cpp
+++ src/producer/producer.cpp
@@ -1,11 +1,53 @@
 #include <iostream>
 #include "pc.h"
 #include "Config.h"
+#include <unistd.h>
+#include <cstdlib>
+
+int ProcessArgs(int argc, char* argv[], int& N, std::string& filename) {
+   int opt;
+   const int kMinNumBufs(10);
+   const int kExpectedArgCount(5);
+
+   if (argc != kExpectedArgCount) {
+      return 1;
+   }
+
+   while ((opt = getopt(argc, argv, "n:f:")) != -1) {
+      switch (opt) {
+         case 'n':
+            N = atoi(optarg);
+            break;
+         case 'f':
+            filename = optarg;
+            break;
+         default:
+            return 2;
+      }
+   }
+
+   if (N < kMinNumBufs) {
+      std::cerr << "Minumun value for -n argument is " << kMinNumBufs << std::endl;
+      return 3;
+   }
+
+   return 0;
+}
 
 int main(int argc, char* argv[]) {
+   int N;
+   std::string filename;
+
+   int res = ProcessArgs(argc, argv, N, filename);
+   if (res != 0) {
+      std::cerr << "Usage: " << argv[0] << " -n [NUM_BUFS] -f [FILE]" << std::endl;
+      exit(res);
+   }
+
+   std::cout << "Num Bufs: " << N << ", filename: " << filename << std::endl;
+
    pc myPc;
    Config myConf("../conf/pcConf");
-
    if (myPc.pcTest()) {
       std::cout << "Producer: Hello pcTest" << std::endl;
    } else {
